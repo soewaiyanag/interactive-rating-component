9936cc57d8791d6e0b606eb2e00dd37f259519a3
Soe Wai Yan Aung <soewaiyanag@gmail.com>
2022-04-01 19:18:24 +0630

    setup the project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import logo from "./logo.svg";
-import "./App.css";
 import { motion } from "framer-motion";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <motion.img
-          src={logo}
-          className="App-logo"
-          alt="logo"
-          drag="x"
-          animate={{ rotate: 180 }}
-          transition={{
-            repeat: Infinity,
-            repeatDelay: 0.5,
-            repeatType: "reverse",
-            duration: 2,
-          }}
-        />
+        <motion.img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
