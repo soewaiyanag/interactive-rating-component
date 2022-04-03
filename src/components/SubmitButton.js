7b2b02b2652d9f90c132dc57af301ce063a0667f
Soe Wai Yan Aung <soewaiyanag@gmail.com>
2022-04-03 11:45:53 +0630

    change isSubmitted state onClick submitButton

--- src/components/SubmitButton.js
+++ src/components/SubmitButton.js
@@ -1,12 +1,18 @@
+import { useContext } from "react";
 import { motion } from "framer-motion";
+import SubmitContext from "../context/SubmitContext";
 
 const SubmitButton = () => {
+  const { setIsSubmitted } = useContext(SubmitContext);
   return (
     <motion.button
       className="
       uppercase w-full p-2 font-semibold
       text-sm tracking-wider transition-colors
       text-white bg-orange-500 rounded-3xl hover:bg-white hover:text-orange-500"
+      onClick={() => {
+        setIsSubmitted(true);
+      }}
     >
       submit
     </motion.button>
